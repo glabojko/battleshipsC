36246d9b01fb39079abfda5808d3f945055321bc
glabojko <glabojko@gmail.com>
2023-07-16 14:42:59 +0200

    first initial commit

---
+++ battleshipC/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace battleshipC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, battlesphips!");
+        }
+    }
+}
