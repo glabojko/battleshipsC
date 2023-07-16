999dc87aafe5e1fa4b72aa99ed8e253ac389706a
glabojko <glabojko@gmail.com>
2023-07-16 14:58:01 +0200

    second commit to test

--- battleshipC/Program.cs
+++ battleshipC/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace battleshipC
+﻿using System;
+
+namespace battleshipC
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, battlesphips!");
+            Console.WriteLine("programmed by Sofiia, Rafał and Grzegorz");
         }
     }
 }
