50a14a7bb8067076d23f8a4fbf7e7ab723a4fb85
glabojko <glabojko@gmail.com>
2023-07-19 21:17:06 +0200

    testing branch

--- battleship/Program.cs
+++ battleship/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, battleships!");
+            Console.WriteLine("Hello, battleships by greg!");
         }
     }
 }
